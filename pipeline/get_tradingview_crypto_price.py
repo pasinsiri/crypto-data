eb0e3c7be7f2a207ce1187ffe83066984962c65c
pasinsiri <pasin_plp0206@hotmail.com>
2025-09-06 14:16:58 +0700

    request data from Supabase

--- pipeline/get_tradingview_crypto_price.py
+++ pipeline/get_tradingview_crypto_price.py
@@ -9,8 +9,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# * load tickers from Supabase
+# * Load cryptocurrency tickers from Supabase
+# ? setup credentials
 TABLE_NAME = "crypto_tickers"
 SUPABASE_PROJECT_ID = os.environ.get("SUPABASE_PROJECT_ID")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 SUPABASE_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/rest/v1/{TABLE_NAME}"
+
+# ? request data from Supabase
+headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+    "Content-Type": "application/json",
+    "Prefer": "return=minimal"
+}
+response = requests.get(SUPABASE_URL, headers=headers)
+assert response.status_code == 200, f'Supabase error: The GET request returns with status {response.status_code}'
+
+tickers = [r['ticker'] for r in response.json()]
