44d8790ea36edf325f7d03b1eb8500fd350f676b
pasinsiri <pasin_plp0206@hotmail.com>
2025-09-06 14:14:56 +0700

    setup Supabase credentials

--- pipeline/get_tradingview_crypto_price.py
+++ pipeline/get_tradingview_crypto_price.py
@@ -8,3 +8,9 @@ from tradingview_screener import Query, Column, And, Or, col
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# * load tickers from Supabase
+TABLE_NAME = "crypto_tickers"
+SUPABASE_PROJECT_ID = os.environ.get("SUPABASE_PROJECT_ID")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+SUPABASE_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/rest/v1/{TABLE_NAME}"
