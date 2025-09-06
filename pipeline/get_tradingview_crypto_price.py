5fc437a4b0a25ad1ec4777121f3329e43c06f689
pasinsiri <pasin_plp0206@hotmail.com>
2025-09-06 14:14:35 +0700

    import libraries

---
+++ pipeline/get_tradingview_crypto_price.py
@@ -0,0 +1,10 @@
+import requests
+import numpy as np
+import pandas as pd
+import datetime as dt
+import os
+import tradingview_screener as tvs
+from tradingview_screener import Query, Column, And, Or, col
+from dotenv import load_dotenv
+
+load_dotenv()
