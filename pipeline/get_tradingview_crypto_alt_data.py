8831f18515d06eeb10cf2abd29a8897068abd652
pasinsiri <pasin_plp0206@hotmail.com>
2025-09-07 10:55:53 +0700

    setup Supabase credentials

--- pipeline/get_tradingview_crypto_alt_data.py
+++ pipeline/get_tradingview_crypto_alt_data.py
@@ -8,3 +8,10 @@ from tradingview_screener import Query, Column, And, Or, col
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# * Load cryptocurrency tickers from Supabase
+# ? setup credentials
+SUPABASE_PROJECT_ID = os.environ.get("SUPABASE_PROJECT_ID")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+TICKERS_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/rest/v1/crypto_alt_tickers"
+TARGET_TABLE_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/rest/v1/crypto_alt_data"
